75c0f3084acd61b018f6f065a53512aae7ce3193
stepan <sssprogramer@gmail.com>
2017-11-14 09:23:44 +0300

    add request to vk.com

--- server.py
+++ server.py
@@ -1,6 +1,7 @@
 from http.server import BaseHTTPRequestHandler, HTTPServer
 import cgi
 import os
+import requests
 
 class MyRequestHandler(BaseHTTPRequestHandler):
     def do_GET(self):
@@ -15,11 +16,19 @@ class MyRequestHandler(BaseHTTPRequestHandler):
         self.send_response(200)
         self.send_header("Content-type", "text/html")
         self.end_headers()
-        header = "You don`t pass acsess_token"
+        header = "You must pass code"
         token = ""
-        if "access_token" in args:
+        if "code" in args:
             header = "Your access_token is"
-            token = args["access_token"][0]
+            url = "https://oauth.vk.com/access_token"
+            params = {"client_id": "6222407",
+                      "client_secret" : "ODTeys6TQsNU9geZtcZ7",
+                      "redirect_uri" : "https://safe-everglades-40623.herokuapp.com/",
+                      "code" : args["code"][0]
+                      }
+            
+            response = requests.get(url, params = params)
+            token = response
         
         self.wfile.write(bytes("<!DOCTYPE html>\
                         <html>\
@@ -32,6 +41,7 @@ class MyRequestHandler(BaseHTTPRequestHandler):
                         </body>\
                         </html>".format(header, token), "utf-8"))
 port = int(os.environ.get("PORT", 5000))
-server = HTTPServer(("0.0.0.0",port), MyRequestHandler)
+host = "localhost"#"0.0.0.0"
+server = HTTPServer((host, port), MyRequestHandler)
 print("server started")
 server.serve_forever()
