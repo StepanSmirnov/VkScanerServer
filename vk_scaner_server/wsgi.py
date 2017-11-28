2283b777d945f211ea82f7a9f83a2f5a14e084c6
stepan <sssprogramer@gmail.com>
2017-11-28 22:42:23 +0300

    fix import

--- vk_scaner_server/wsgi.py
+++ vk_scaner_server/wsgi.py
@@ -1,5 +1,6 @@
 from django.core.wsgi import get_wsgi_application
 from whitenoise.django import DjangoWhiteNoise
+import os
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "vk_scaner_server.settings")
 
