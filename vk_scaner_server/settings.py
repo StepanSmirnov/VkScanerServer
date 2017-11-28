de1cce7c0ecfe889067c86b95a18467b6ab72616
stepan <sssprogramer@gmail.com>
2017-11-28 22:45:22 +0300

    change alowed hosts

--- vk_scaner_server/settings.py
+++ vk_scaner_server/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '+p=o3cdknzj=pbtc)$4tm^(4&!q&5tfce##rc1z8bre^=)w9av'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['safe-everglades-40623.herokuapp.com']
 
 
 # Application definition
