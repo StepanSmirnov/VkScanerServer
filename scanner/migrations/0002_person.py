4d4ce4de0da8a9e1b339ab14283d7677ecb2c576
stepan <sssprogramer@gmail.com>
2017-12-06 22:55:19 +0300

    fix names

--- scanner/migrations/0002_person.py
+++ scanner/migrations/0002_person.py
@@ -8,7 +8,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('scaner', '0001_initial'),
+        ('scanner', '0001_initial'),
     ]
 
     operations = [
--- scanner/migrations/0003_photo.py
+++ scanner/migrations/0003_photo.py
@@ -9,7 +9,7 @@ import django.db.models.deletion
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('scaner', '0002_person'),
+        ('scanner', '0002_person'),
     ]
 
     operations = [
