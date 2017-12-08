08a128cebdfb9e72928747b03665311dee6828b1
stepan <sssprogramer@gmail.com>
2017-12-08 10:16:44 +0300

    fix double registration

--- scanner/admin.py
+++ scanner/admin.py
@@ -2,5 +2,5 @@ from django.contrib import admin
 from .models import Person, Photo, PhotoAdmin
 
 admin.site.register(Person)
-admin.site.register(Photo, PhotoAdmin)
+# admin.site.register(Photo, PhotoAdmin)
 # Register your models here.
